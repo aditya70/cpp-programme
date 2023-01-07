0ea500eff19fdb4df4fb0bda612ef52e67ec8c4c
aditya <anitp2018@gmail.com>
2023-01-07 16:22:55 -0700

    conatiner with most water

--- Leetcode/_0011_Container_With_Most_Water.cpp
+++ Leetcode/_0011_Container_With_Most_Water.cpp
@@ -3,6 +3,14 @@ using namespace std;
 class Solution {
 public:
     int maxArea(vector<int>& height) {
-        
+        int a=0,n=height.size(),f=0,l=n-1,hf,hl;
+        while(f<l){
+            hf=height[f];
+            hl=height[l];
+            a=max(a,min(hf,hl)*(l-f));
+            if(hf>hl) l--;
+            else f++;
+        }
+        return a;
     }
 };
