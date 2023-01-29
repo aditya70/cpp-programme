d208bb4e2a0a3973be126eb7799a04c906768634
aditya <agoyal61@asu.edu>
2023-01-28 20:37:26 -0700

    fixed time limit exceeded limit

--- Leetcode/_0474_Ones_And_Zeroes.cpp
+++ Leetcode/_0474_Ones_And_Zeroes.cpp
@@ -2,24 +2,25 @@
 using namespace std;
 class Solution {
 public:
+    int dp[601][101][101];
     int count0(string s){
         int c=0;
         for(char& ch:s)if(ch=='0')c++;
         return c;
     }
-    int solve(vector<string> s, int m, int n, int idx, vector<vector<vector<int>>>& v){
+    int solve(vector<string>& s, int m, int n, int idx){
         if(idx==s.size() || m+n==0)return 0;
-        if(v[m][n][idx]>0)return v[m][n][idx];
+        if(dp[idx][m][n]!=-1)return dp[idx][m][n];
         int zero=count0(s[idx]);
         int one=s[idx].size()-zero;
         int inc=0;
-        if(zero<=m && one<=n)inc=1+solve(s,m-zero,n-one,idx+1,v);
-        int excl=solve(s,m,n,idx+1,v);
-        v[m][n][idx]=max(inc,excl);
-        return v[m][n][idx];
+        if(zero<=m && one<=n)inc=1+solve(s,m-zero,n-one,idx+1);
+        int excl=solve(s,m,n,idx+1);
+        return dp[idx][m][n]=max(inc,excl);
     }
+ 
     int findMaxForm(vector<string>& strs, int m, int n) {
-        vector<vector<vector<int>>> dp(m+1,vector<vector<int>> (n+1, vector<int> (strs.size()+1,0)));
-        return solve(strs, m, n, 0, dp);
+        memset(dp,-1,sizeof(dp));
+        return solve(strs, m, n, 0);
     }
 };
